95794c121285ecb7e251fd7b051b772e2eb356cc
millzhang <876753183@qq.com>
2017-11-03 10:28:16 +0800

    '修改路由导航'

--- src/router/index.js
+++ src/router/index.js
@@ -15,10 +15,17 @@ export default new Router({
   }, {
     path: '/canvas/wave',
     name: 'canvas实现波浪效果',
-    component: CanvasWave
+    component: CanvasWave,
+    meta: {
+      cover: 'http://www.wpthemegenerator.com/wp-content/uploads/2012/06/Image.jpg',
+      desc: '使用 canvas 实现波浪效果的原理与 SVG 一样，都是利用路径绘制出三次贝塞尔曲线并赋予动画效果'
+    }
   }, {
     path: '/canvas/firework',
     name: 'canvas实现烟花',
-    component: require('views/canvas/canvas-firework')
+    component: require('views/canvas/canvas-firework'),
+    meta: {
+      desc: '使用 canvas 实现烟花效果,鼠标点击可释放烟花'
+    }
   }]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     <div class="list">
-      <template v-for="item in list">
-        <router-link class="wrap" :to="item.path">
-          <h1>{{item.title}}</h1>
+      <template v-for="item in $router.options.routes">
+        <router-link class="wrap" :to="item.path" v-if="item.path != '/'">
+          <h1>{{item.name}}</h1>
           <div class="image">
-            <img :src="item.img">
+            <img :src="item.meta.cover == undefined ? defaultImage:item.meta.cover">
           </div>
-          <p>{{item.desc}}</p>
+          <p>{{item.meta.desc == undefined ? item.name : item.meta.desc}}</p>
         </router-link>
       </template>
     </div>
@@ -19,24 +19,16 @@ export default {
   name: 'hello',
   data() {
     return {
-      list: [],
-      msg: 'Welcome to Your Vue.js App'
+      defaultImage: 'http://g.hiphotos.baidu.com/image/pic/item/b3fb43166d224f4a1687bf6603f790529822d1ad.jpg'
     }
   },
   created() {
-    this.fetchMenuList();
+    // this.fetchMenuList();
   },
   methods: {
-    fetchMenuList() {
-      axios.get('/api/menu').then(result => {
-        if (result.status == 200) {
-          this.list = result.data.data;
-        }
-      })
-    }
+
   }
 }
 
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped src="./../assets/styles/home.scss"></style>
